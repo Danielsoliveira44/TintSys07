a06a0b1fe30574218b82c7e7d77e072d03b81a89
danielsoliveira44 <ds5676419@gmail.com>
2023-04-14 08:37:33 -0300

    cliente

--- TintSysClass/Endereco.cs
+++ TintSysClass/Endereco.cs
@@ -1,13 +1,135 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TintSysClass;
 
-namespace TintSysClass
+namespace ClassLibrary2
 {
     public class Endereco
     {
-        
+        private int id;
+        private int cep;
+        private string logradouro;
+        private int numero;
+        private string complemento;
+        private string bairro;
+        private string cidade;
+        private string estado;
+        private string uf;
+        private string tipo;
+
+        public int Id { get => id; set => id = value; }
+        public int Cep { get => cep; set => cep = value; }
+        public string Logradouro { get => logradouro; set => logradouro = value; }
+        public int Numero { get => numero; set => numero = value; }
+        public string Complemento { get => complemento; set => complemento = value; }
+        public string Bairro { get => bairro; set => bairro = value; }
+        public string Cidade { get => cidade; set => cidade = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public string Uf { get => uf; set => uf = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+
+
+        // Métodos contrutores 
+        public Endereco() { } // vazio
+        public Endereco(int id, int cep, string logradouro, int numero, string complemento, string bairro, string cidade, string estado,
+            string uf, string tipo)
+        {
+            Id = id;
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+            Uf = uf;
+            Tipo = tipo;
+
+        }
+        public Endereco(int id, string logradouro, int numero, string bairro, string cidade)
+        {
+            Id = id;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+
+        }
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandText = "insert usuarios (cep, logradouro, numero, complemento, bairro, cidade, estado, uf, tipo), " +
+                "@cep, @logradouro, @numero, @complemento, @bairro, @cidade, @estado, @uf, @tipo) ";
+            cmd.Parameters.AddWithValue("@cep", Cep);
+            cmd.Parameters.AddWithValue("@logradouro", Logradouro);
+            cmd.Parameters.AddWithValue("@numero", Numero);
+            cmd.Parameters.AddWithValue("@complemento", Complemento);
+            cmd.Parameters.AddWithValue("@bairro", Bairro);
+            cmd.Parameters.AddWithValue("@cidade", Cidade);
+            cmd.Parameters.AddWithValue("@estado", Estado);
+            cmd.Parameters.AddWithValue("@uf", Uf);
+            cmd.Parameters.AddWithValue("@tipo", Tipo);
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "select @@identity";
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
+            Banco.Fechar(cmd);
+        }
+
+        public static Endereco ObterPorId(int _id)
+        {
+            Endereco endereco = null;
+            var cmd = Banco.Abrir();
+            cmd.CommandText = "select * from enderecos where id = " + _id;
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                endereco = new Endereco(
+                        dr.GetInt32(0),
+                        dr.GetInt32(1),
+                        dr.GetString(2),
+                        dr.GetInt32(3),
+                        dr.GetString(4),
+                        dr.GetString(5),
+                        dr.GetString(6),
+                        dr.GetString(7),
+                        dr.GetString(8),
+                        dr.GetString(9)
+                    );
+            }
+            Banco.Fechar(cmd);
+            return endereco;
+        }
+
+        public static List<Endereco> Listar(string _nome = "")
+        {
+            List<Endereco> lista = new List<Endereco>();
+
+            var cmd = Banco.Abrir();
+            if (_nome != string.Empty)
+                cmd.CommandText = "select * from enderecos where logradouro like '%" + _nome + "%'";
+            else
+                cmd.CommandText = "select * from enderecos";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                lista.Add(new Endereco(
+                         dr.GetInt32(0),
+                        dr.GetString(1),
+                        dr.GetInt32(2),
+                        dr.GetString(3),
+                        dr.GetString(4)
+                    ));
+            }
+            Banco.Fechar(cmd);
+            return lista;
+        }
+
+
     }
 }
--- TintSysClass/Telefone.cs
+++ TintSysClass/Telefone.cs
@@ -1,12 +1,118 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TintSysClass;
 
-namespace TintSysClass
+namespace ClassLibrary2
 {
-    internal class Telefone
+    public class Telefone
     {
+        // atributos
+
+        private int id;
+        private string numero;
+        private string tipo;
+
+        // Propriedades (Encapsulamento) getters and setters
+        public int Id { get => id; set => id = value; }
+        public string Numero { get => numero; set => numero = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+
+        // Métodos contrutores 
+        public Telefone()
+        {
+
+        }
+        public Telefone(int id, string numero, string tipo)
+        {
+            Id = id;
+            Numero = numero;
+            Tipo = tipo;
+
+        }
+        public Telefone(string numero, string tipo)
+        {
+            Numero = numero;
+            Tipo = tipo;
+
+        }
+        // Métodos da Classes (inserir, alterar, consultar,por Id, por nome, etc.... )
+        public void Inserir()
+        {
+            // cria uma variável com conexão de banco aberta
+            var cmd = Banco.Abrir();
+            // define o tipo de instrução MySQL a ser processada pelo serv banco dados 
+            cmd.CommandType = CommandType.Text;
+            // define a query sql especificada com parametros ()
+            cmd.CommandText = "insert telefones (numero, tipo, cliente) values (@numero, @tipo, @cliente)";
+            // cria o parametro e associa ao valor
+            cmd.Parameters.AddWithValue("@numero", Numero);
+            cmd.Parameters.AddWithValue("@tipo", Tipo);
+            //cmd.Parameters.AddWithValue("@cliente", Cliente.Id);
+
+            // executa a instrução SQL na conexão
+            cmd.ExecuteNonQuery();
+            // obtendo o id do nível recém inserido
+            cmd.CommandText = "select @@identity";
+            // recupera o id na Propriedade
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
+            // fecha a conexão
+            Banco.Fechar(cmd);
+        }
+        public static Telefone ObterPorId(int _id)
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from telefones where id = @id";
+            cmd.Parameters.AddWithValue("@id", _id);
+            var dr = cmd.ExecuteReader();
+            Telefone telefone = null;
+            while (dr.Read())
+            {
+                telefone = new Telefone(
+                    dr.GetInt32(0),
+                    dr.GetString(1),
+                    dr.GetString(2)
+                    );
+            }
+            Banco.Fechar(cmd);
+            return telefone;
+        }
+        public static List<Telefone> Listar(string _nome = "")
+        {
+            List<Telefone> lista = new List<Telefone>();
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            if (_nome != string.Empty)
+                cmd.CommandText = "select * from telefones like '%" + _nome + "%'";
+            else
+                cmd.CommandText = "select * from telefones order by numero";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                lista.Add(new Telefone(dr.GetInt32(0),
+                        dr.GetString(1),
+                        dr.GetString(2)
+                    )
+                );
+            }
+            Banco.Fechar(cmd);
+            return lista;
+        }
+        public void Atualizar()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update telefones set numero = @numero, tipo = @tipo where id = @id";
+            cmd.Parameters.AddWithValue("@id", Id);
+            cmd.Parameters.AddWithValue("@numero", Numero);
+            cmd.Parameters.AddWithValue("@tipo", Tipo);
+            cmd.ExecuteNonQuery();
+            Banco.Fechar(cmd);
+        }
     }
 }
